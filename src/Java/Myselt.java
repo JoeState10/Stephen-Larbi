02740695b766171afbab061462787245db887ca6
waddle1212 <abugrisilas5@gmail.com>
2026-06-24 13:17:45 -0800

    Work 1

---
+++ src/Java/Myselt.java
@@ -0,0 +1,33 @@
+package Java;
+
+class AboutMyself {
+    public static  void main(String[] args) {
+        // Input code
+        String name = "Larbi Stephen";
+        int age = 21;
+        String Nationality = "Ghanaian";
+        String location = "Koforidua, Eastern Region, Ghana";
+        String hobby = "Gaming and Football";
+        String favoriteGame = "eFootball";
+        String favouritefood = "Fufu";
+
+
+        // Display code
+
+        System.out.println("o_o>>>>>>> Myself <<<<<<<o_o");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Nationality:" + Nationality);
+        System.out.println("Location: " + location);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Game: " + favoriteGame);
+        System.out.println("Favouritefood"+ favouritefood);
+
+
+
+        // Fun message
+        System.out.println("\n I enjoy to do on my free time is to do " +
+                " a bit reading and gaming");
+        System.out.println("Nice to meet you!");
+    }
+}
